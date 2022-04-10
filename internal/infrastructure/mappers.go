097eb9910df9f5c64c0108355cc4bac87435fe03
Orkun Gürsel <orkungrsl@gmail.com>
2022-04-11 02:00:50 +0300

    feat: add util functions for mapping

---
+++ internal/infrastructure/mappers.go
@@ -0,0 +1,34 @@
+package infrastructure
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/orkungursel/hey-taxi-location-api/internal/domain/model"
+)
+
+func MapRedisGeoLocationToDomain(g redis.GeoLocation) *model.Location {
+	return &model.Location{
+		Driver: g.Name,
+		Lat:    g.Latitude,
+		Lng:    g.Longitude,
+		Dist:   g.Dist,
+	}
+}
+
+func MapLocationToRedisGeoLocation(l model.Location) *redis.GeoLocation {
+	return &redis.GeoLocation{
+		Name:      l.Driver,
+		Longitude: l.Lng,
+		Latitude:  l.Lat,
+		Dist:      l.Dist,
+	}
+}
+
+func MapUserToDriver(u model.User) *model.Driver {
+	return &model.Driver{
+		Id:       u.Id,
+		Name:     u.Name,
+		Nickname: u.Nickname,
+		Email:    u.Email,
+		Picture:  u.Picture,
+	}
+}
