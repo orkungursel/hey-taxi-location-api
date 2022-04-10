88c8863b490d076a39b52e2050df46898e998e8b
Orkun Gürsel <orkungrsl@gmail.com>
2022-04-11 02:01:12 +0300

    feat: add request and response structs

---
+++ internal/app/request.go
@@ -0,0 +1,12 @@
+package app
+
+// TODO validate lat/lng
+type SaveLocationRequest struct {
+	Lat float64 `json:"lat" validate:"required,gte=-90,lte=90"`
+	Lng float64 `json:"lng" validate:"required,gte=-180,lte=180"`
+} // @name SaveLocationRequest
+
+type SearchLocationRequest struct {
+	Lat float64 `json:"lat" validate:"required"`
+	Lng float64 `json:"lng" validate:"required"`
+} // @name SearchLocationRequest
---
+++ internal/app/response.go
@@ -0,0 +1,16 @@
+package app
+
+import "github.com/orkungursel/hey-taxi-location-api/internal/domain/model"
+
+type HTTPError struct {
+	Code     int         `json:"-"`
+	Message  interface{} `json:"message"`
+	Internal error       `json:"-"` // Stores the error returned by an external dependency
+} // @name HTTPError
+
+type LocationResponse struct {
+	Driver model.Driver `json:"driver"`
+	Lat    float64      `json:"lat"`
+	Lng    float64      `json:"lng"`
+	Dist   float64      `json:"dist"`
+} // @name LocationResponse
