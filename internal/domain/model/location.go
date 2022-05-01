4e238f6161543b32f1e0f60460d456ef41abd298
Orkun Gürsel <orkungrsl@gmail.com>
2022-05-02 02:33:44 +0300

    feat: update location model to use vehicle id
    it was using driver id before.

--- internal/domain/model/location.go
+++ internal/domain/model/location.go
@@ -1,8 +1,8 @@
 package model
 
 type Location struct {
-	Driver string  `json:"driver" validate:"required"`
-	Lat    float64 `json:"lat" validate:"required,gte=-90,lte=90"`
-	Lng    float64 `json:"lng" validate:"required,gte=-180,lte=180"`
-	Dist   float64 `json:"dist"`
+	VehicleId string  `json:"vehicle_id" validate:"required"`
+	Lat       float64 `json:"lat" validate:"required,gte=-90,lte=90"`
+	Lng       float64 `json:"lng" validate:"required,gte=-180,lte=180"`
+	Dist      float64 `json:"dist"`
 }
